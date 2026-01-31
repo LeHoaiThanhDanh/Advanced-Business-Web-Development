2503fbd981c8cf4dacc4d800300a555bd47a530b
Lê Hoài Thành Danh <titusle123@gmail.com>
2026-01-31 12:25:17 +0700

    Exercise 19

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,21 @@ import { Ptb1 } from './ptb1/ptb1';
 import { Learnbinding } from './learnbinding/learnbinding';
 import { Listcustomer } from './listcustomer/listcustomer';
 import { Ex14catelog } from './exercise14/exercise14';
+import { Ex18 } from './exercise18/exercise18';
+import { Ex13 } from './exercise13/exercise13'; 
+import { Product } from './exercise19/product/product';
+import { ListProduct } from './exercise19/list-product/list-product';
+import { ServiceProduct } from './exercise19/service-product/service-product';
 
 export const routes: Routes = [
   { path: 'ptb1', component: Ptb1 },
   { path: 'learnbinding', component: Learnbinding },
   { path: 'listcustomer', component: Listcustomer },
   { path: 'Exercise14', component: Ex14catelog },
-  { path: '', redirectTo: '/ptb1', pathMatch: 'full' }
+  { path: 'Exercise18', component: Ex18 },
+  { path: 'Exercise13', component: Ex13 },
+  { path: 'Product', component: Product },
+  { path: 'ListProduct', component: ListProduct },
+  { path: 'ServiceProduct', component: ServiceProduct },
+  { path: '', redirectTo: '/Exercise14', pathMatch: 'full' }
 ];
