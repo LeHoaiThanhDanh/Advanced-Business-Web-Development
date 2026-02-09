7f08a861ad6cc2c1587e6b8e71711d16c226aa1a
Lê Hoài Thành Danh <titusle123@gmail.com>
2026-02-09 19:25:23 +0700

    Exercise 50

---
+++ src/app/exercise50/exercise50.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercise50',
+  imports: [],
+  templateUrl: './exercise50.html',
+  styleUrl: './exercise50.css',
+})
+export class Exercise50 {
+
+}
